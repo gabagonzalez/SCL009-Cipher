c0421cfce524915c814192cbfdc71c79c3357437
gabagonzalez <gaba2801@gmail.com>
2019-04-03 16:29:59 -0300

    encode

--- src/cipher.js
+++ src/cipher.js
@@ -1,34 +1,47 @@
 window.cipher = {
-  function encode() {
-    encode:(user_offset, user_text) =>{
+  encode(offset,text){
+    let output = '';  
+    let ascii = '';
+    for (let i = 0; i< text.length; i++){
+    acsii = text[i].charCodeAt(0);
+    console.log(ascii);
+    /*en código acsii*/  
+    let letter;
+    if (acsii  >=65 && acsii<= 90){
+    letter = String.fromCharCode((acsii- 65 + offset)%26 + 65);
+    /*de la A-Z*/
+    }else if (97 >= acsii&& acsii<= 122){
+    letter = String.fromCharCode((acsii- 97 + offset)%26 + 97);
+    /*de la a-z*/
+    }/*else{
+    letter = String.fromCharCode(acsii);
+    }*/
+      output += letter
+    }
+    return output;
+  },
+  /*
+    decode(offset,text)=> {
       let output = '';  
-  
-      for (let i = 0; i< user_text.length; i++){
-      let acsii_letter = user_text.charCodeAt(i);
-      /*en código acsii*/  
-      let newLetter;
-      if (acsii_letter >=65 && acsii_letter <= 90){
-      newLetter = String.fromCharCode((acsii_letter - 65 + user_offset)%26 + 65);
-      /*de la A-Z*/
-      }else if (97 >= acsii_letter && acsii_letter <= 122){
-      newLetter = String.fromCharCode((acsii_letter - 97 + user_offset)%26 + 97);
-      /*de la a-z*/
-      }else{
-      newLetter = String.fromCharCode(acsii_letter);
+      let ascii = '';
+      for (let i = 0; i< text.length; i++){
+      acsii = text[i].charCodeAt(0);
+      console.log(ascii);
+      -en código acsii*/  
+      /*
+      let letter;
+      if (acsii  >=65 && acsii<= 90){
+      letter = String.fromCharCode((acsii- 65 + offset)%26 + 65);
+      -de la A-Z*/
+      /*
+      }else if (97 >= acsii&& acsii<= 122){
+      letter = String.fromCharCode((acsii- 97 + offset)%26 + 97);
+      - la a-z*/
+      /*}else{
+      letter = String.fromCharCode(acsii);
       }
-        output += newLetter
+        output += letter
       }
       return output;
-      }
-    };
-   
-  /*
-  function encode()
-  decode: () => {
-    /* Acá va tu código */
-  }
-}
-
-
-
-};*/
+  }*/
+};
--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
 //audio
 let audio = document.getElementById("audio");
 audio.play();
+//let 
+//let name;
+//Trae el texto contenido en nuestra caja de texto^
+// document.getElementById("principal").style.display = "none";
+// document.getElementById("formulario").style.display = "block";
 
-let name;
-function goPressed() {
-  //función que se ejecuta al presionar el botón enviar
-  name = document.getElementById("boxName").value;
-  //Trae el texto contenido en nuestra caja de texto^
-  document.getElementById("principal").style.display = "none";
-  document.getElementById("formulario").style.display = "block";
-  //trae los elementos por su id, "sacando" el principal y mostrando formulario
-  prompt = "Hola " + name + " Aquí escribe tu mensaje ";
-  //reasigna title concatenando el nombre ingresado//
-}
+// function goPressed() {
+//   //función que se ejecuta al presionar el botón enviar
+//   name = document.getElementById("boxName").value;
+//   
+//   //trae los elementos por su id, "sacando" el principal y mostrando formulario
+//   prompt = "Hola " + name + " Aquí escribe tu mensaje ";
+//   //reasigna title concatenando el nombre ingresado//
+// }
