63729b1935b02a6f6c33156d7402c7fa73199be2
gabagonzalez <gaba2801@gmail.com>
2019-04-03 23:05:19 -0300

    ok cipher.js

--- src/cipher.js
+++ src/cipher.js
@@ -20,28 +20,25 @@ window.cipher = {
     }
     return output;
   },
-  /*
-    decode(offset,text)=> {
-      let output = '';  
-      let ascii = '';
-      for (let i = 0; i< text.length; i++){
-      acsii = text[i].charCodeAt(0);
-      console.log(ascii);
-      -en código acsii*/  
-      /*
-      let letter;
-      if (acsii  >=65 && acsii<= 90){
-      letter = String.fromCharCode((acsii- 65 + offset)%26 + 65);
-      -de la A-Z*/
-      /*
-      }else if (97 >= acsii&& acsii<= 122){
-      letter = String.fromCharCode((acsii- 97 + offset)%26 + 97);
-      - la a-z*/
-      /*}else{
-      letter = String.fromCharCode(acsii);
-      }
-        output += letter
-      }
-      return output;
-  }*/
+    decode(offset,text){
+    let output = '';  
+    let ascii = '';
+    for (let i = 0; i< text.length; i++){
+    acsii = text[i].charCodeAt(0);
+    console.log(ascii);
+    /*en código acsii*/  
+    let letter;
+    if (acsii  >=65 && acsii<= 90){
+    letter = String.fromCharCode((acsii+ 65 - offset)%26 + 65);
+    /*de la A-Z*/
+    }else if (97 >= acsii&& acsii<= 122){
+    letter = String.fromCharCode((acsii+ 97 - offset)%26 + 97);
+    /*de la a-z*/
+    }/*else{
+    letter = String.fromCharCode(acsii);
+    }*/
+      output += letter
+    }
+    return output;
+  },
 };
