e7fd1884f589d4d26b7d1381f521dfb4ff04e656
gabagonzalez <gaba2801@gmail.com>
2019-04-05 03:50:30 -0300

    subiendo cambios

--- src/cipher.js
+++ src/cipher.js
@@ -1,42 +1,63 @@
 window.cipher = {
-  encode(offset,text){
-    let output = '';  
-    let ascii = '';
-    for (let i = 0; i< text.length; i++){
-    acsii = text[i].charCodeAt(0);
-    /*en código acsii*/  
-    let letter;
-    if (acsii  >=65 && acsii<= 90){
-    letter = String.fromCharCode((acsii- 65 + offset)%26 + 65);
-    /*de la A-Z*/
-    }else if (97 >= acsii&& acsii<= 122){
-    letter = String.fromCharCode((acsii- 97 + offset)%26 + 97);
-    /*de la a-z*/
-    }/*else{
-    letter = String.fromCharCode(acsii);
-    }*/
-      output += letter
-    }
-    return output;
-  },
-    decode(offset,text){
-    let output = '';  
-    let ascii = '';
-    for (let i = 0; i< text.length; i++){
-    acsii = text[i].charCodeAt(0);
-    /*en código acsii*/  
-    let letter;
-    if (acsii  >=65 && acsii<= 90){
-    letter = String.fromCharCode((acsii+ 65 - offset)%26 + 65);
-    /*de la A-Z*/
-    }else if (97 >= acsii&& acsii<= 122){
-    letter = String.fromCharCode((acsii+ 97 - offset)%26 + 97);
-    /*de la a-z*/
-    }/*else{
-    letter = String.fromCharCode(acsii);
-    }*/
-      output += letter
-    }
-    return output;
-  },
+encode: (toEncode,offSet) => {
+let codAscii = ''; 
+let cifrado;
+let fin = '';
+if (toEncode == "" || offSet == "") {
+alert("Falta datos!!");
+for (let i = 0; i<= toEncode.length; i++){
+let toAscii = toEncode.charCodeAt(i);
+/*en código acsii*/  
+/*if (toAscii === 165){
+cifrado = String.fromCharCode(codAscii);
+descifrado += cifrado;
+console.log(cifrado)
+Ñ*/
+}
+if (toAscii  >=65 && toAscii<= 90){
+cifrado = String.fromCharCode((codAscii- 65 + offSet)%26 + 65);
+descifrado += cifrado;
+/*de la A-Z*/
+}/*else if (toAscii === 32){
+codAscii = (toAscii- 32 + offSet)%1 + 32);
+((espacio))  
+cifrado = String.fromCharCode(codAscii);
+descifrado += cifrado;
+console.log(descifrado)
+}else if (97 >= acsii&& acsii<= 122){
+cifrado = String.fromCharCode((codAscii- 97 + offSet)%26 + 97);
+--de la a-z
+}/*else{
+cifrado = String.fromCharCode(acsii);
+}*/
+}
+return descifrado;
+},
+
+decode: (toDecode, offSet) => {
+let descifrado = '';  
+let codAscii = '';
+let fin2 = '';
+if (toDecode == "" || offSet == "") {
+alert("Falta datos!!");
+for (let i = 0; i< toDecode.length; i++){
+codAscii = toDecode.charCodeAt(i);
+/*en código acsii*/  
+if (toAscii  >=65 && toAscii<= 90){
+descifrado = String.fromCharCode((toAscii+ 65 - offSet)%26 + 65);
+descifrado += descifrado;
+/*de la A-Z*/
+}else if(toAscii === 32){
+  codAscii = (toAscii + 32 - offSet) % 1 + 32;
+  descifrado = String.fromCharCode(codAscii);
+  descifrado += cifrado;
+/*else if (97 >= toAscii&& toAscii<= 122){
+descifrado = String.fromCharCode((toAscii+ 97 - offSet)%26 + 97);
+/*de la a-z*/
+}/*else{
+letter = String.fromCharCode(acsii);
+}*/
+fin2 += descifrado;
+}
+return fin2;
 };
--- src/index.js
+++ src/index.js
@@ -1,50 +1,39 @@
-// audio
-let audio = document.getElementById("audio");
-audio.play();
-
-// innerHTML con addEventListener
-const offset= document.getElementById("offset");
-// Guardo en una variable el elemento number que luego ocupare en addEventListener
-const ulText = document.getElementById("list")
-// Guardo en una variable el elemento li (elemento madre)traido desde HTML 
-ulText.appendChild(litext);
-// Mi elemento hijo vacio mientras
-
-btnShow.addEventListener('click', () => {
-let text = document.getElementById("text").value
-let btn = '';
-// Guardo en una variable los datos ingresados
-let resultado;
-for(var i = 0; i < text.length; i++){
-//Si boton1 es cifrar 
-if ("boton1"){
-resultado = cipher.encode();
-//window.cipher ,cipher.encode()
-}else if ("boton2"){
-    resultado = cipher.decode();
-// Si boton2 es decifrar
-/*
-
-//decifrar 
-cipher.decode()window.cipher */
-    }
-      output += resultado
-    }
-    return output;
-  },  
-ulText.innerHTML += <li class="texto"> ${output[i] } </li>
-// Le digo que se imprima en la etiqueta ul y que por cada iteración agregue un li
-};
-
-
-
-// let name;
-// Trae el texto contenido en nuestra caja de texto
-// document.getElementById("principal").style.display = "none";
-// document.getElementById("formulario").style.display = "block";
-// función que se ejecuta al presionar el botón enviar
-// name = document.getElementById("boxName").value;
-// prompt = "Hola " + name + " Comencemos a Cifrar!! ";
-// reasigna title concatenando el nombre ingresado
-//console.log(text);
-//console.log(text[i]);
+  /*audio*/
+  let audio = document.getElementById("audio");
+  /*innerHTML con addEventListener*/
+  document.getElementById("cifrarBtn").addEventListener("click", () => {
+  const toEncode = document.getElementById("textArea1").value.toUpperCase();
+  /*Guardo en una variable el texto ingresados*/
+  const offSet = parseInt(document.getElementById("inputOffSet").value);
+  /*Guardo en una variable el elemento offset*/
+  let result = window.cipher.encode(toEncode,offSet)
+  document.getElementById("textArea2").innerHTML = result;
+  });
+  document.getElementById("descifrarBtn").addEventListener("click", () => {
+  const toDecode = document.getElementById("textArea1").value.toUpperCase();
+  /*Guardo en una variable el texto ingresados*/
+  const offSet = parseInt(document.getElementById("inputOffSet").value);
+  /*Guardo en una variable el elemento offset*/
+  let result = src/cipher.js(cipher.decode(toDecode,offSet))
+  document.getElementById("textArea2").innerHTML = result;
+});
+document.getElementById("resetbtn").addEventListener("click", () => {
+  document.getElementById("textArea1").value = "";
+  document.getElementById("inputOffSet").value = "";
+  document.getElementById("textArea2").value = "";
+});
+/*const ulText = document.getElementById("list")
+Guardo en una variable el elemento li (elemento madre)traido desde HTML 
+ulText.appendChild(litext);Mi elemento hijo vacio mientras.ulText.innerHTML += <li class="texto"> ${output[i] } </li>
+Le digo que se imprima en la etiqueta ul 
+y que por cada iteración agregue un li
+let name;
+Trae el texto contenido en nuestra caja de texto
+document.getElementById("principal").style.display = "none";
+document.getElementById("formulario").style.display = "block";
+función que se ejecuta al presionar el botón enviar
+name = document.getElementById("boxName").value;
+prompt = "Hola " + name + " Comencemos a Cifrar!! ";
+reasigna title concatenando el nombre ingresado
+console.log(text);
+console.log(text[i]);*/
