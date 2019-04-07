8e91e4eb6d21eda7f898bdc38d3e06172a85e891
gabagonzalez <gaba2801@gmail.com>
2019-04-07 20:31:07 -0300

    subiendo cipher avanzado

--- src/cipher.js
+++ src/cipher.js
@@ -1,68 +1,41 @@
-  window.cipher = {    
+window.cipher = {    
   encode: (offset, toEncode) => { 
     let result1 = '';
-    //let codAscii = '';
+
     for(let i = 0; i< toEncode.length; i++){
     let toAscii = toEncode.charCodeAt(i);
-    console.log(toAscii);
+    //console.log(toAscii);
     if (toAscii>=65 && toAscii<=90){
     toUnicode = String.fromCharCode((toAscii- 65 + offset)%26 + 65);
     result1 += toUnicode;
-    // /*de la A-Z*/
-    }
-    
+    }else if (toAscii === 32){
+    toAscii = ((toAscii- 32 + offset)%1 + 32);
+    toUnicode1  = String.fromCharCode(toAscii);
+    result1 += toUnicode1;
+    }//else{
+    //result1 += (toEncode);
+    //}
+
   }
   return result1;
-  }
-}
-  /*decode: (offset,toDecode) => {
-  let toDecode = '';  
-  let codAscii = '';
-  let result2 = '';
-  if (toDecode == "" || offset == "") {
-  alert("Falta datos!!");
-  }for (let i = 0; i< toDecode.length; i++){
-  codAscii = toDecode.charCodeAt(i);
-  en código acsii
-  if (toAscii  >=65 && toAscii<= 90){
-  toDecode = String.fromCharCode((toAscii+ 65 - offset-26)%26 + 65);
-  result2 += toDecode;
-  /*de la A-Z
+  },
+  decode: (offset, toDecode) => { 
+    let result2 = '';
+    //let codAscii = '';
+    for(let i = 0; i< toDecode.length; i++){
+    let toAscii = toDecode.charCodeAt(i);
+    if (toAscii>=65 && toAscii<=90){
+    toUnicode2 = String.fromCharCode((toAscii+ 65 - offset-26)%26 + 65);
+    result2 += toUnicode2;
+    }else if (toAscii === 32){
+    toAscii = ((toAscii- 32 + offset)%1 + 32);
+    toUnicode3  = String.fromCharCode(toAscii);
+    result2 += toUnicode3;
+    }//else{
+    //result2 += (toDecode);
+    //}
   }
   return result2;
-
-  };*/
-
-    /*if (toAscii === 165){
-  cifrado = String.fromCharCode(codAscii);
-  descifrado += cifrado;
-  console.log(cifrado)
-  Ñ*/
-  /*else if (toAscii === 32){
-  codAscii = (toAscii- 32 + offset)%1 + 32);
-  ((espacio))
-  /*toEncode  = String.fromCharCode(codAscii);
-  result += toEncode;
-  console.log(descifrado)
-
-  }else if (97 >= acsii&& acsii<= 122){
-  toEncode  = String.fromCharCode((codAscii- 97 + offset)%26 + 97);
-  --de la a-z
-
-  }else{
-  toEncode = String.fromCharCode(acsii);
-
-  --
-  else if(toAscii === 32){
-    codAscii = (toAscii + 32 - offset) % 1 + 32;
-    descifrado = String.fromCharCode(codAscii);
-    descifrado += cifrado;
-  /*else if (97 >= toAscii&& toAscii<= 122){
-  descifrado = String.fromCharCode((toAscii+ 97 - offset)%26 + 97);
-  de la a-z
-  }else{
-  letter = String.fromCharCode(acsii);
+  }
+};
   
-}
-}
-  }*/
--- src/index.js
+++ src/index.js
@@ -1,50 +1,42 @@
-  /*audio
-  let audio = document.getElementById("audio");
-  innerHTML con addEventListener
-  1-Guardo en una variable el texto ingresados
-  Guardo en una variable el elemento offset
-  let result = window.cipher.encode(toEncode,offset)
-  document.getElementById("containerResult").innerHTML = result;
-  2-Guardo en una variable el texto ingresados
-  Guardo en una variable el elemento offset
-  let result = src/cipher.js(cipher.decode(toDecode,offset))
-  document.getElementById("textArea2").innerHTML = result;*/
+//audio /document.getElementById("audio").value;
+function myFunction() {
+  var x = document.getElementById("audio").autoplay;
+  document.getElementById("demo").innerHTML = x;
+}
+//let name;
+//function namePressed() {
+//name = document.getElementById("boxName").value;
+//document.getElementById("comencemos").style.display = "none";
+//document.getElementById("cifrar").style.display = "block";
+//let title = document.getElementById("instruccion");
+//title.innerHTML = "¡¡ " + name + " estas son las Instrucciones..!!";
+//}
   let containerResult = document.getElementById('container');
   
 document.getElementById("btnCifrar").addEventListener("click", () => {
   let toEncode= document.getElementById("message").value.toUpperCase();
   let offset = parseInt(document.getElementById("offset").value);
   containerResult.innerHTML = cipher.encode(offset,toEncode);
-  // if (toEncode != ""){
-    
-  // }
-  // else{
-  //   alert("No hay texto")
-  // }
+  if (toEncode != ""){
+  }
+  else{
+  alert("Faltan Datos :(")
+  }
   });
 document.getElementById("btnDescifrar").addEventListener("click", () => {
-  const toDecode = document.getElementById("message").value.toUpperCase();
-  const offset = parseInt(document.getElementById("offset").value);
+  let toDecode = document.getElementById("message").value.toUpperCase();
+  let offset = parseInt(document.getElementById("offset").value);
   containerResult.innerHTML = cipher.decode(offset,toDecode);
+  if (toDecode != ""){
+  }
+  else{
+  alert("Faltan Datos :(")
+  }
 });
 document.getElementById("resetbtn").addEventListener("click", () => {
   document.getElementById("message").value = "";
   document.getElementById("offset").value = "";
-  document.getElementById("containerResult").value = "";
+  document.getElementById("container").value = "";
 
 });
-/*const ulText = document.getElementById("list")
-Guardo en una variable el elemento li (elemento madre)traido desde HTML 
-ulText.appendChild(litext);Mi elemento hijo vacio mientras.ulText.innerHTML += <li class="texto"> ${output[i] } </li>
-Le digo que se imprima en la etiqueta ul 
-y que por cada iteración agregue un li
-let name;
-Trae el texto contenido en nuestra caja de texto
-document.getElementById("principal").style.display = "none";
-document.getElementById("formulario").style.display = "block";
-función que se ejecuta al presionar el botón enviar
-name = document.getElementById("boxName").value;
-prompt = "Hola " + name + " Comencemos a Cifrar!! ";
-reasigna title concatenando el nombre ingresado
-console.log(text);
-console.log(text[i]);*/
+
